cde7b58a9d2658facc231bd929fd1140f42c447b
ys3124 <y.wlmu3124@gmail.com>
2024-01-21 11:19:04 +0900

    Create housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css',

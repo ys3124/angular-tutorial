8ee8e824fb1f16c4332867ade4dd74cf99a1d579
ys3124 <y.wlmu3124@gmail.com>
2024-01-21 15:29:01 +0900

    Add dynamic values to templates

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -5,7 +5,19 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
-  template: ` <p>housing-location works!</p> `,
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
   styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {

9165b18e9fd7ea704c9cd263ec63fa1a769318bd
ys3124 <y.wlmu3124@gmail.com>
2024-01-21 12:47:54 +0900

    Add inputs to component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrl: './housing-location.component.css'
+  template: ` <p>housing-location works!</p> `,
+  styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
